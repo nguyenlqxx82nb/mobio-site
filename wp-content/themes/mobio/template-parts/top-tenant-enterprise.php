a00830e9dd24b93305a9f0e632043fd07661a1d9
Nguyen Le <nguyenlqvn1@gmail.com>
2022-03-17 11:40:44 +0700

    Fix top tenant

--- wp-content/themes/mobio/template-parts/top-tenant-enterprise.php
+++ wp-content/themes/mobio/template-parts/top-tenant-enterprise.php
@@ -4,10 +4,25 @@
       'classExtend' => $args['classExtend'],
       'title' => $args['title'] ?  $args['title'] : 'trusted by top enterprises in vietnam',
       'items' => array(
-        array('img-src' => get_template_directory_uri(). '/images/top-tenant/vib.png'),
-        array('img-src' => get_template_directory_uri(). '/images/top-tenant/samsung.png'),
-        array('img-src' => get_template_directory_uri(). '/images/top-tenant/vnlife.png'),
-        array('img-src' => get_template_directory_uri(). '/images/top-tenant/flc.png'),
-      )
+          array(get_template_directory_uri(). '/images/top-tenant/vib.png', 
+                get_template_directory_uri(). '/images/top-tenant/samsung.png',
+                get_template_directory_uri(). '/images/top-tenant/vnlife.png',
+                get_template_directory_uri(). '/images/top-tenant/flc.png') ,
+
+          array(get_template_directory_uri(). '/images/top-tenant/vib.png', 
+                get_template_directory_uri(). '/images/top-tenant/samsung.png',
+                get_template_directory_uri(). '/images/top-tenant/vnlife.png',
+                get_template_directory_uri(). '/images/top-tenant/flc.png') ,
+
+          array(get_template_directory_uri(). '/images/top-tenant/vib.png', 
+                get_template_directory_uri(). '/images/top-tenant/samsung.png',
+                get_template_directory_uri(). '/images/top-tenant/vnlife.png',
+                get_template_directory_uri(). '/images/top-tenant/flc.png') ,
+
+          array(get_template_directory_uri(). '/images/top-tenant/vib.png', 
+                get_template_directory_uri(). '/images/top-tenant/samsung.png',
+                get_template_directory_uri(). '/images/top-tenant/vnlife.png',
+                get_template_directory_uri(). '/images/top-tenant/flc.png') ,
+        )
     ));
 ?>
